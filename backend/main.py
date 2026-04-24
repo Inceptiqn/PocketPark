39dbdf928c8f068206a2982301ec899448f1c1e3
Inceptiqn <aceacewastaken@gmail.com>
2026-04-24 13:18:29 +0200

    routes initial setup

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+from routes import api
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# All /api/* routes live in routes.py
+app.register_blueprint(api)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
